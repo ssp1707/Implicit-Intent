08d724bf3ac14dbf8ee951d3bd0adcd1cb5100e3
S. Sanjna Priya <75234965+ssp1707@users.noreply.github.com>
2022-04-27 10:53:54 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/exp02/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.exp02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(view -> {
+            String url=editText.getText().toString();
+            Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+    }
+}
